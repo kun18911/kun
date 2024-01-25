4bea45f534b77c20918478c876f97e5caba961d1
kun18911 <trunghieu99u@gmail.com>
2024-01-25 14:16:39 +0700

    first commit

---
+++ bomb.js
@@ -0,0 +1,30 @@
+var Bomb = function (row, col, color) {
+  Candy.call(this, row, col, color);
+
+  this.type = 'bomb';
+};
+
+Bomb.prototype = Object.create(Candy.prototype);
+Bomb.prototype.constructor = Bomb;
+
+Bomb.prototype.toString = function () {
+  return 'B';
+};
+z
+Bomb.prototype.handleCrush = function () {
+  var startRow = Math.max(0, this.row - 1);
+  var endRow = Math.min(board.boardSize - 1, this.row + 1);
+  var startCol = Math.max(0, this.col - 1);
+  var endCol = Math.min(board.boardSize - 1, this.col + 1);
+
+  for (var row = startRow; row <= endRow; row++) {
+      for (var col = startCol; col <= endCol; col++) {
+          var candy = board.getCandyAt(row, col);
+          if (candy) {
+              board.remove(candy);
+          }
+      }
+  }
+
+  board.remove(this);
+};
---
+++ candy.js
@@ -0,0 +1,26 @@
+var Candy = function(color, id)
+{
+  Object.defineProperty(this, 'color', {value: color, writable: false});
+  Object.defineProperty(this, 'id', {value: id, writable: false});
+
+  this.row = null;
+  this.col = null;
+
+  this.isColorBomb = color === 'red';
+
+  this.toString = function()
+  {
+    var name = this.color;
+    return name;
+  }
+};
+
+
+Candy.colors = [
+  'red',
+  'yellow',
+  'green',
+  'orange',
+  'blue',
+  'purple'
+];
